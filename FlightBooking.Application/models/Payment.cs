fd275640702122e6f98cc56fa2bcc388d077a1ec
Aaron Domingo <DOM22825@spengergasse.at>
2022-12-29 21:45:34 +0100

    Added CalculateTotalPrice

--- FlightBooking.Application/models/Payment.cs
+++ FlightBooking.Application/models/Payment.cs
@@ -10,11 +10,10 @@ namespace FlightBooking.models
     [Table("Payment")]
     public class Payment
     {
-        public Payment(Booking booking, double paymentAmount, DateTime paymentDate)
+        public Payment(Booking booking, DateTime paymentDate)
         {
             Booking = booking;
             BookingId = booking.Id;
-            PaymentAmount = paymentAmount;
             PaymentDate = paymentDate;
         }
 
@@ -25,8 +24,25 @@ namespace FlightBooking.models
         public int Id { get; private set; }
         public Booking Booking { get; set; }
         public int BookingId { get; set; }
-        public double PaymentAmount { get; set; }
         public DateTime PaymentDate { get; set; }
+        public double CalculateTotalPrice(Booking booking)
+        {
+            if (booking != null)
+            {
+                double flightPrice = booking.Flight.Price;
+                double baggagePrice = 0;
 
+                foreach (Baggage b in booking.Baggages)
+                {
+                    baggagePrice += b.Price;
+                }
+
+                return flightPrice + baggagePrice;
+            }
+            else
+            {
+                throw new Exception("No booking was specified");
+            }
+        }
     }
 }
