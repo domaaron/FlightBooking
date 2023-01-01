a2500cc4140e92d1e5625379c141a9630c8bfd8e
Aaron Domingo <DOM22825@spengergasse.at>
2023-01-01 15:45:30 +0100

    Abstract class for Flight

---
+++ FlightBooking.Application/models/FlightStatus.cs
@@ -0,0 +1,28 @@
+﻿using FlightBooking.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightBooking.Application.models
+{
+    public class FlightStatus : Flight
+    {
+        public FlightStatus(Flight flight, string status) 
+            :base(flight.DepartureTime, flight.ArrivalTime, flight.DestinationTime, flight.Airplane, flight.DepartureAddress, flight.ArrivalAddress, flight.Price)
+        {
+            Status = status;
+        }
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        protected FlightStatus() { }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+        public string Status { get; set; }
+        public void ChangeStatus(string status)
+        {
+            Status = status;
+        }
+    }
+}
