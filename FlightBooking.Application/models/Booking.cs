4e8338be10cd1e6cb6cd89eb0cfd20a7a1c53c83
Aaron Domingo <DOM22825@spengergasse.at>
2023-01-01 15:44:35 +0100

    Added property PaymentMethod() and Guid

--- FlightBooking.Application/models/Booking.cs
+++ FlightBooking.Application/models/Booking.cs
@@ -8,9 +8,12 @@ using System.Threading.Tasks;
 
 namespace FlightBooking.models
 {
+    public enum FlightClass { Economy, Business, First};
+
     [Table("Booking")]
     public class Booking
     {
+        private FlightClass _class;
         private readonly List<Baggage> _baggages = new();
         public Booking(Passenger passenger, Flight flight, string seatNumber, FlightClass flightClass)
         {
@@ -19,15 +22,17 @@ namespace FlightBooking.models
             Flight = flight;
             FlightId = flight.Id;
             SeatNumber = seatNumber;
-            FlightClass = flightClass;
+            this._class = flightClass;
             DateOfBooking = DateTime.Now;
+            Guid = Guid.NewGuid();
         }
 
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         protected Booking() { }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
-        public int Id { get; set; }
+        public int Id { get; private set; }
+        public Guid Guid { get; private set; }
         public Passenger Passenger { get; set; }
         public virtual int PassengerId { get; private set; }
         public Flight Flight { get; set; }
@@ -35,6 +40,7 @@ namespace FlightBooking.models
         public string SeatNumber { get; set; }
         public FlightClass FlightClass { get; set; }
         public DateTime DateOfBooking { get; }
+        public string PaymentMethod { get; private set; } = default!;
         public IReadOnlyCollection<Baggage> Baggages => _baggages;
 
         public void AddBaggage(Booking booking, Baggage baggage)
