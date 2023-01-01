86ddba1f8be2706c2f5f528ed1395955a619f44a
Aaron Domingo <DOM22825@spengergasse.at>
2023-01-01 15:44:15 +0100

    Minor changes

--- FlightBooking.Application/models/Baggage.cs
+++ FlightBooking.Application/models/Baggage.cs
@@ -21,7 +21,7 @@ namespace FlightBooking.models
         protected Baggage() { }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
-        public int Id { get; set; }
+        public int Id { get; private set; }
         public string Name { get; set; }
         public double Weight { get; set; }
         public double Price { get; set; }
--- FlightBooking.Application/models/Producer.cs
+++ FlightBooking.Application/models/Producer.cs
@@ -16,11 +16,13 @@ namespace FlightBooking.models
             Name = name;
         }
 
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         protected Producer() { }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
-        public string Name { get; set; }
+        public string Name { get; private set; }
 
     }
 }
